aefc18f97f3845930c7abbc21ad12c61b992e5aa
hoppertim17 <hoppertim17@gmail.com>
2020-01-04 08:45:37 -0600

    Commit for chapter 2 exercises

---
+++ Chapter 2/hello_world.py
@@ -0,0 +1,26 @@
+message = 'Hello Python world!'
+print(message)
+
+message = 'Hello Python Crash Course world!'
+print(message)
+
+name = 'ada lovelace'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f'{first_name} {last_name}'
+print(full_name)
+print(f'Hello, {full_name.title()}')
+
+print('\tPython')
+print('Languages:\n\tPython\n\tC\n\tJavaScript')
+
+
+favorite_language = ' python '
+print(f'"{favorite_language}"')
+print(f'"{favorite_language.rstrip()}"')
+print(f'"{favorite_language.lstrip()}"')
+print(f'"{favorite_language.strip()}"')
